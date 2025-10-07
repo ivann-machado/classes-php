9ed70d673b2ee78cdff29b6ef28b1301b5106f36
Ivann <ivann.machado@laplateforme.io>
2025-10-07 11:38:52 +0200

    reindent + fix

--- User.php
+++ User.php
@@ -1,123 +1,132 @@
 <?php
-	$db = mysqli_connect('localhost', 'root', '', 'classes');
-	class User {
-		private $id;
-		public $login;
-		public $email;
-		public $firstname;
-		public $lastname;
+$db = mysqli_connect('localhost', 'root', '', 'classes');
+class User {
+	private $id;
+	public $login;
+	public $email;
+	public $firstname;
+	public $lastname;
 
-		public function __construct() {
-			if (isset($_SESSION['user_id'])) {
-				$user = $this->getAllInfos($_SESSION['user_id']);
-				$this->id = $user['id'];
-				$this->login = $user['login'];
-				$this->email = $user['email'];
-				$this->firstname = $user['firstname'];
-				$this->lastname = $user['lastname'];
-			}
+	public function __construct() {
+		if (isset($_SESSION['user_id'])) {
+			$user = $this->getAllInfos($_SESSION['user_id']);
+			$this->id = $user['id'];
+			$this->login = $user['login'];
+			$this->email = $user['email'];
+			$this->firstname = $user['firstname'];
+			$this->lastname = $user['lastname'];
 		}
+	}
 
-		public function register($login, $password, $email, $firstname, $lastname) {
+	public function register($login, $password, $email, $firstname, $lastname) {
+		$result = mysqli_query($db, 'SELECT * FROM `users` WHERE `login` = "'.htmlspecialchars($login).'"');
+		if (mysqli_num_rows($result) > 0) {
+			throw new Exception('Login already taken.');
+		}
+		else {
 			$query = 'INSERT INTO `users` (`login`, `password`, `email`, `firstname`, `lastname`) VALUES ("'.htmlspecialchars($login).'", "'.password_hash($password, PASSWORD_BCRYPT).'", "'.htmlspecialchars($email).'", "'.htmlspecialchars($firstname).'", "'.htmlspecialchars($lastname).'")';
 			if (mysqli_query($db, $query)) {
 				return getAllInfos(mysqli_insert_id($db));
 			}
 		}
+	}
 
-		public function connect($login, $password) {
-			$result = mysqli_query($db, 'SELECT * FROM `users` WHERE `login` = "'.htmlspecialchars($login).'"');
-			if (mysqli_num_rows($result) == 1) {
-				$user = mysqli_fetch_assoc($result);
-				if (password_verify($password, $user['password'])) {
-					$this->id = $user['id'];
-					$this->login = $user['login'];
-					$this->email = $user['email'];
-					$this->firstname = $user['firstname'];
-					$this->lastname = $user['lastname'];
-					return true;
-				}
-				else {
-					throw new Exception('Incorrect password.');
-				}
+	public function connect($login, $password) {
+		$result = mysqli_query($db, 'SELECT * FROM `users` WHERE `login` = "'.htmlspecialchars($login).'"');
+		if (mysqli_num_rows($result) == 1) {
+			$user = mysqli_fetch_assoc($result);
+			if (password_verify($password, $user['password'])) {
+				$this->id = $user['id'];
+				$this->login = $user['login'];
+				$this->email = $user['email'];
+				$this->firstname = $user['firstname'];
+				$this->lastname = $user['lastname'];
+				return true;
 			}
 			else {
-				throw new Exception('User not found.');
-			}
-
-		}
-
-		public function disconnect() {
-			$this->id = null;
-			$this->login = null;
-			$this->email = null;
-			$this->firstname = null;
-			$this->lastname = null;
-		}
-
-		public function delete() {
-			if (mysqli_query($db, 'DELETE FROM `users` WHERE `id` = "'.$this->id.'"')) {
-				$this->disconnect();
+				throw new Exception('Incorrect password.');
 			}
 		}
-
-		public function update($login, $password = null, $email = null, $firstname = null, $lastname = null) {
-			$fields = [];
-			if ($login !== null) {
-				$fields[] = '`login` = "'.htmlspecialchars($login).'"';
-			}
-			if ($password !== null) {
-				$fields[] = '`password` = "'.password_hash($password, PASSWORD_BCRYPT).'"';
-			}
-			if ($email !== null) {
-				$fields[] = '`email` = "'.htmlspecialchars($email).'"';
-			}
-			if ($firstname !== null) {
-				$fields[] = '`firstname` = "'.htmlspecialchars($firstname).'"';
-			}
-			if ($lastname !== null) {
-				$fields[] = '`lastname` = "'.htmlspecialchars($lastname).'"';
-			}
-			if (!empty($fields)) {
-				$query = 'UPDATE `users` SET '.implode(', ', $fields).' WHERE `id` = "'.$this->id.'"';
-				if (mysqli_query($db, $query)) {
-					$this->login = $login;
-					$this->email = $email;
-					$this->firstname = $firstname;
-					$this->lastname = $lastname;
-				}
-			}
+		else {
+			throw new Exception('User not found.');
 		}
 
-		public function isConnected() {
-			return isset($this->id);
-		}
+	}
 
-		public function getAllInfos($id = null) {
-			$id = $id ?? $this->id;
-			$result = mysqli_query($db, 'SELECT * FROM `users` WHERE `id` = "'.htmlspecialchars($id).'"');
-			if (mysqli_num_rows($result) == 1) {
-				return mysqli_fetch_assoc($result);
-			}
-			else {
-				throw new Exception('User not found.');
-			}
-		}
+	public function disconnect() {
+		$this->id = null;
+		$this->login = null;
+		$this->email = null;
+		$this->firstname = null;
+		$this->lastname = null;
+	}
 
-		public function getLogin() {
-			return $this->login;
-		}
-
-		public function getEmail() {
-			return $this->email;
-		}
-
-		public function getFirstname() {
-			return $this->firstname;
-		}
-
-		public function getLastname() {
-			return $this->lastname;
+	public function delete() {
+		if (mysqli_query($db, 'DELETE FROM `users` WHERE `id` = "'.$this->id.'"')) {
+			$this->disconnect();
 		}
 	}
+
+	public function update($login, $password = null, $email = null, $firstname = null, $lastname = null) {
+		$fields = [];
+		if ($login !== null) {
+			$fields[] = '`login` = "'.htmlspecialchars($login).'"';
+		}
+		if ($password !== null) {
+			$fields[] = '`password` = "'.password_hash($password, PASSWORD_BCRYPT).'"';
+		}
+		if ($email !== null) {
+			$fields[] = '`email` = "'.htmlspecialchars($email).'"';
+		}
+		if ($firstname !== null) {
+			$fields[] = '`firstname` = "'.htmlspecialchars($firstname).'"';
+		}
+		if ($lastname !== null) {
+			$fields[] = '`lastname` = "'.htmlspecialchars($lastname).'"';
+		}
+		if (!empty($fields)) {
+			$query = 'UPDATE `users` SET '.implode(', ', $fields).' WHERE `id` = "'.$this->id.'"';
+			if (mysqli_query($db, $query)) {
+				$this->login = $login;
+				$this->email = $email;
+				$this->firstname = $firstname;
+				$this->lastname = $lastname;
+			}
+		}
+	}
+
+	public function isConnected() {
+		return isset($this->id);
+	}
+
+	public function getAllInfos($id = null) {
+		$id = $id ?? $this->id;
+		$result = mysqli_query($db, 'SELECT * FROM `users` WHERE `id` = "'.htmlspecialchars($id).'"');
+		if (mysqli_num_rows($result) == 1) {
+			return mysqli_fetch_assoc($result);
+		}
+		else {
+			throw new Exception('User not found.');
+		}
+	}
+
+	public function getLogin() {
+		return $this->login;
+	}
+
+	public function getEmail() {
+		return $this->email;
+	}
+
+	public function getFirstname() {
+		return $this->firstname;
+	}
+
+	public function getLastname() {
+		return $this->lastname;
+	}
+}
+
+$user = new User();
+$user->register('test', 'password', 'test@email.com', 'Test', 'User');
 ?>
