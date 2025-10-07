5cdd25e26f5b9786076febf9455531164a072191
Ivann <ivann.machado@laplateforme.io>
2025-10-07 12:01:06 +0200

    User => Userpdo, isConnected

--- user-pdo.php
+++ user-pdo.php
@@ -1,63 +1,63 @@
 <?php
-	$db = mysqli_connect('localhost', 'root', '', 'classes');
-	class User {
-		private $id;
-		public $login;
-		public $email;
-		public $firstname;
-		public $lastname;
+$db = mysqli_connect('localhost', 'root', '', 'classes');
+class Userpdo {
+	private $id;
+	public $login;
+	public $email;
+	public $firstname;
+	public $lastname;
 
-		public function __construct() {
+	public function __construct() {
 			// code...
-		}
-
-		public function register($login, $password, $email, $firstname, $lastname) {
-
-		}
-
-		public function connect($login, $password) {
-
-		}
-
-		public function disconnect() {
-			$this->id = null;
-			$this->login = null;
-			$this->email = null;
-			$this->firstname = null;
-			$this->lastname = null;
-		}
-
-		public function delete() {
-
-			$this->disconnect();
-		}
-
-		public function update($login, $password = null, $email = null, $firstname = null, $lastname = null) {
-			// code...
-		}
-
-		public function isConnected() {
-			// code...
-		}
-
-		public function getAllInfos() {
-			// code...
-		}
-
-		public function getLogin() {
-			return $this->login;
-		}
-
-		public function getEmail() {
-			return $this->email;
-		}
-
-		public function getFirstname() {
-			return $this->firstname;
-		}
-
-		public function getLastname() {
-			return $this->lastname;
-		}
 	}
+
+	public function register($login, $password, $email, $firstname, $lastname) {
+
+	}
+
+	public function connect($login, $password) {
+
+	}
+
+	public function disconnect() {
+		$this->id = null;
+		$this->login = null;
+		$this->email = null;
+		$this->firstname = null;
+		$this->lastname = null;
+	}
+
+	public function delete() {
+
+		$this->disconnect();
+	}
+
+	public function update($login, $password = null, $email = null, $firstname = null, $lastname = null) {
+			// code...
+	}
+
+	public function isConnected() {
+		return isset($this->id);
+	}
+
+	public function getAllInfos() {
+			// code...
+	}
+
+	public function getLogin() {
+		return $this->login;
+	}
+
+	public function getEmail() {
+		return $this->email;
+	}
+
+	public function getFirstname() {
+		return $this->firstname;
+	}
+
+	public function getLastname() {
+		return $this->lastname;
+	}
+}
 ?>
